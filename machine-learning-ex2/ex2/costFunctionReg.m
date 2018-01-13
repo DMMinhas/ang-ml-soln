1c41415b966d241cd8650467885fa17401a999aa
Jixin Feng <ufjfeng@users.noreply.github.com>
2018-01-13 00:12:46 -0500

    add ex2

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% J = 0;
+% grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -17,11 +17,18 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+z = X * theta;
+h = sigmoid(z);
 
+logisf = (-y)' * log(h) - (1 - y)' * log(1 - h);
+reg = (lambda / 2 / m) * sum(theta .^ 2);
 
+J = (1 / m) * sum(logisf) + reg;
 
+grad_zero = (1 / m) * X(:, 1)' * (h - y);
+grad_res = (1 / m) * (X(:, 2:length(theta))' * (h - y)) + lambda / m * theta(2:length(theta));
 
-
+grad = [grad_zero; grad_res];
 % =============================================================
 
 end
